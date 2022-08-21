ac50b5a95b509081b2899e6bbaa0178ce420128f
Wal72Thela <walt2thela@gmail.com>
2022-08-21 14:28:42 +0200

    new file:   log.html 	new file:   log.js

---
+++ log/log.js
@@ -0,0 +1,5 @@
+var firstname = "Nhlanhla"
+var institution = "Ekurhuleni East"
+var gitUserName = "Wal72Thela"
+
+console.log("My name is "+firstname+ " I'm a student of " + institution+ " and my Github Username is " +gitUserName)
